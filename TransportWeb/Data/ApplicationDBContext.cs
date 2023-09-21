dab15c54efca2586a4d49eada6d0e46d12ed26fb
vmorunta <73287937+vmorunta@users.noreply.github.com>
2023-09-21 18:06:24 +0300

    Add project files.

---
+++ TransportWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TransportWeb.Models;
+
+namespace TransportWeb.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options) { 
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+    }
+}
---
+++ TransportWeb/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace TransportWeb.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public DateTime CreatedDateTime {  get; set; } 
+    }
+}
